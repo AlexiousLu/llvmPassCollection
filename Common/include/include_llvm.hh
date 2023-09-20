bf355d2f819bd9e02dc29e0e47da39da8b050df3
ZhipengLu <smyalexied@gmail.com>
2023-09-20 16:46:43 +0800

    add include_llvm.hh

---
+++ Common/include/include_llvm.hh
@@ -0,0 +1,6 @@
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/InstIterator.h>
+#include <llvm/Support/SourceMgr.h>
