f45e53ca15fce08ad943b3d57c5f51bca68d3d48
Zhipeng Lu <lu000728@umn.edu>
2024-08-30 16:42:02 -0500

    fix: using clang as compiler and remove wrong llvm-include

--- Common/cmake/common_cmake.cmake
+++ Common/cmake/common_cmake.cmake
@@ -5,8 +5,8 @@ include_directories("$ENV{MYENVS}/vcpkg/installed/x64-linux/include")
 if(${CMAKE_PROJECT_NAME} STREQUAL DefaultModule)
     message(FATAL_ERROR "Please change ProjectName")
 endif()
-set(CMAKE_C_COMPILER "gcc-8")
-set(CMAKE_CXX_COMPILER "g++-8")
+# set(CMAKE_C_COMPILER "gcc-8")
+# set(CMAKE_CXX_COMPILER "g++-8")
 set(CMAKE_BUILD_TYPE "Debug")
 set(CMAKE_CXX_STANDARD 20)
 
@@ -14,13 +14,11 @@ message(STATUS "CMAKE_MODULE_PATH = ${CMAKE_MODULE_PATH}")
 message(STATUS "CMAKE_ROOT = ${CMAKE_ROOT}")
 message(STATUS "Current path: ${CMAKE_CURRENT_SOURCE_DIR}")
 
-set(LLVM_PATH /home/zhipeng/my_envs/llvm-project/llvm)
-link_directories(${LLVM_PATH}/lib)
-include_directories(${LLVM_PATH}/include)
 
 find_package(LLVM 15.0.0 REQUIRED CONFIG) #  PATHS /home/zhipeng/my_envs/llvm-15 NO_DEFAULT_PATH
 message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
 message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
+message(STATUS "Found LLVM include dir: ${LLVM_INCLUDE_DIRS}")
 add_definitions(${LLVM_DEFINITIONS})
 include_directories(${LLVM_INCLUDE_DIRS})
 set(LLVM_COLLECTION
